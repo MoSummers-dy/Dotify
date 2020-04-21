b6ac37dc603625b0bdec9b6af3590db1699ee7b5
Diana Dai <dianadai.dy@gmail.com>
2020-04-21 15:54:24 -0700

    complete part 1

--- app/src/main/java/edu/washington/dy2018/dotify/SongListAdapter.kt
+++ app/src/main/java/edu/washington/dy2018/dotify/SongListAdapter.kt
@@ -9,28 +9,30 @@ import androidx.recyclerview.widget.RecyclerView
 import com.ericchee.songdataprovider.Song
 
 class SongListAdapter(initialListOfSongs: List<Song>) :RecyclerView.Adapter<SongListAdapter.SongViewHolder>() {
-    private var listOfSongs: List<Song> = initialListOfSongs.toList();
+    private var listOfSongs: List<Song> = initialListOfSongs.toList()
     var onSongClickListener: ((song: Song) -> Unit) ?= null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):SongViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_song, parent, false)
 
-        return SongViewHolder(view);
+        return SongViewHolder(view)
     }
 
     override fun getItemCount() = listOfSongs.size
 
-    override fun onBindViewHolder(holder: SongListAdapter.SongViewHolder, position: Int): Unit {
+    override fun onBindViewHolder(holder: SongViewHolder, position: Int) {
         val song = listOfSongs[position]
-        holder.bind(song, position)
+        holder.bind(song)
     }
 
     class SongViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
         private val tvSongName = itemView.findViewById<TextView>(R.id.tvSongName)
+        private val tvArtistName = itemView.findViewById<TextView>(R.id.tvArtistName)
         private val ivSongCover = itemView.findViewById<ImageView>(R.id.ivSongImage)
 
-        fun bind(song: Song, position: Int) {
+        fun bind(song: Song) {
             tvSongName.text = song.title
+            tvArtistName.text = song.artist
             ivSongCover.setImageResource(song.smallImageID)
         }
     }
